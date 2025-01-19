58da83dad54e0d94d77d9f212e0627dcb179e9ab
KGebski0036 <kgebski68@gmail.com>
2025-01-19 18:26:51 +0000

    test

--- scripts/analyze_logs.py
+++ scripts/analyze_logs.py
@@ -40,10 +40,13 @@ wrong_interpret_brute = 0
 
 for i, predicted_class in enumerate(predicted_classes.tolist()):
     probabilities_for_element = probabilities[i].tolist()
-    if (probabilities_for_element[2] > 0.99 and labels[i] != 2):
+    if (probabilities_for_element[2] > 0.60 and labels[i] != 2):
         wrong_interpret_normal += 1
-    if (probabilities_for_element[2] < 0.99 and labels[i] == 2):
+    if (probabilities_for_element[2] < 0.60 and labels[i] == 2):
+        print()
         print(probabilities_for_element[2])
+        print("\033[31m" + f"Warning, probable bruteforce attack attempt from IP: {labels[i]}" + "\033[0m")
+        print(data[i])
         wrong_interpret_brute += 1
     if (probabilities_for_element[0] > 0.5):
         class_counts[0] += 1
@@ -53,10 +56,8 @@ for i, predicted_class in enumerate(predicted_classes.tolist()):
         # print("\033[33m")
         # print(*data[i].split(), sep='\n')
         # print("\033[0m")
-    if (probabilities_for_element[2] > 0.90):
-        # print(data[i])
+    if (probabilities_for_element[2] > 0.60):
         # print("\033[31m" + f"Warning, probable bruteforce attack attempt from IP: {labels[i]}" + "\033[0m")
-        # print("\033[33m" + f"POST Requests Number {len(data[i].split())} {probabilities_for_element[2]}" + "\033[0m")
         class_counts[2] += 1
 
 print("\033[34m" + f"Summary: Normal activity: {class_counts[0]}, SQL Injection: {class_counts[1]}, brouteforce: {class_counts[2]}" + "\033[0m")
